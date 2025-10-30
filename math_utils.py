70070bbe220be348016e3d81436c11c2d4f2ab13
norsultani <norsultani@gmail.com>
2025-10-30 13:39:25 +0200

    Refactor equal number checks in math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -6,8 +6,8 @@ def find_max_number(num1, num2, num3):
     elif num3 > num1 >= num2 or num3 > num2 >= num1:
       return num3
     elif num1 == num3 or num1 == num2 or num2 == num3 and num1 == num3 :
-      return "equal numbers:", num1
+      return num1
     elif num1 == num3 or num1 == num2 or num2 == num3 and num2 == num1 :
-      return "equal numbers:", num2
+      return num2
     elif num1 == num3 or num1 == num2 or num2 == num3 and num2 == num3 :
-      return "equal numbers:", num2
+      return num2
