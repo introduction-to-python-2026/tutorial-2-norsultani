2b8143a99f0c4d05f3f16db3650d87eea18cdc02
norsultani <norsultani@gmail.com>
2025-10-30 13:36:16 +0200

    Implement find_max_number function logic

--- math_utils.py
+++ math_utils.py
@@ -1,2 +1,13 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    if num1 > num2 >= num3 or num1 > num3 >= num2:
+      return num1
+    elif num2 > num1 >= num3 or num2 > num3 >= num1:
+      return num2
+    elif num3 > num1 >= num2 or num3 > num2 >= num1:
+      return num3
+    elif num1 == num3 or num1 == num2 or num2 == num3 and num1 == num3 :
+      return "equal numbers:", num1
+    elif num1 == num3 or num1 == num2 or num2 == num3 and num2 == num1 :
+      return "equal numbers:", num2
+    elif num1 == num3 or num1 == num2 or num2 == num3 and num2 == num3 :
+      return "equal numbers:", num2
